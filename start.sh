048d162e7c179dbe4d1162d335fa0d5fadb67ec7
Alyx Ferrari <rubiquikyt@gmail.com>
2022-02-18 18:00:42 -0800

    Fix QEMU script

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ qemu-system-x86_64 \
     -vga std \
     -machine q35 \
     -m 128M \
-    -drive if=pflash,format=raw,readonly=on,file=/Users/alyxferrari/uefi-test/firmware/OVMF_CODE.fd \
-    -drive if=pflash,format=raw,file=/Users/alyxferrari/uefi-test/firmware/OVMF_VARS-1024x768.fd \
-    -drive format=raw,file=fat:rw:/Users/alyxferrari/uefi-test/target/x86_64-unknown-uefi/debug \
+    -drive if=pflash,format=raw,readonly=on,file=./firmware/OVMF_CODE.fd \
+    -drive if=pflash,format=raw,file=./firmware/OVMF_VARS-1024x768.fd \
+    -drive format=raw,file=fat:rw:./target/x86_64-unknown-uefi/debug \
     -serial stdio \
     -monitor vc:1024x768 \
