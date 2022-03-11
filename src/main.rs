cc045d2e4fc429d027caec012d80bf96d3ae7eec
Alyx Ferrari <rubiquikyt@gmail.com>
2022-03-11 10:18:44 -0500

    Add print statements

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use uefi::prelude::*;
 pub extern "efiapi" fn efi_main(_image: Handle, mut system_table: SystemTable<Boot>) {
     uefi_services::init(&mut system_table).expect_success("Failed to initialize utilities"); // UEFI init
     info!("Hello, world!"); // print "Hello, world!" to the screen
+    info!("UEFI Hello World example using rust-osdev/uefi-rs");
+    info!("by Alyx Ferrari")
     loop {} // loop {asm!("hlt");} would use less energy
 }
 
